0d5830f6733325367e77d638f69f99feff31a61a
Marcos Vinicius de Souza <mageramarcos@gmail.com>
2025-11-04 19:54:51 -0300

    Inicio: Catalago de produtos

---
+++ avaliacao01.js
@@ -0,0 +1,78 @@
+const catalogoProdutos = [{
+        id: 1,
+        nome: "Laptop Gamer Nitro 5",
+        categoria: "Eletrônicos",
+        preco: 5500.00,
+        emEstoque: true
+    },
+    {
+        id: 2,
+        nome: "Mouse Logitech G203",
+        categoria: "Acessórios",
+        preco: 149.90,
+        emEstoque: true
+    },
+    {
+        id: 3,
+        nome: "Monitor Curvo Samsung 27'",
+        categoria: "Eletrônicos",
+        preco: 1800.00,
+        emEstoque: false
+    },
+    {
+        id: 4,
+        nome: "Cadeira de Escritório Ergonômica",
+        categoria: "Móveis",
+        preco: 1200.50,
+        emEstoque: true
+    },
+    {
+        id: 5,
+        nome: "Teclado Mecânico Redragon",
+        categoria: "Acessórios",
+        preco: 350.00,
+        emEstoque: true
+    },
+    {
+        id: 6,
+        nome: "SSD Kingston 1TB",
+        categoria: "Eletrônicos",
+        preco: 450.00,
+        emEstoque: false
+    },
+    {
+        id: 7,
+        nome: "Webcam Full HD",
+        categoria: "Acessórios",
+        preco: 280.00,
+        emEstoque: true
+    },
+    {
+        id: 8,
+        nome: "Mesa de Escritório em L",
+        categoria: "Móveis",
+        preco: 799.99,
+        emEstoque: true
+    },
+    {
+        id: 9,
+        nome: "Pacote 500 Folhas Sulfite A4",
+        categoria: "Papelaria",
+        preco: 29.90,
+        emEstoque: true
+    },
+    {
+        id: 10,
+        nome: "Kit Canetas Coloridas",
+        categoria: "Papelaria",
+        preco: 15.50,
+        emEstoque: false
+    },
+    {
+        id: 11,
+        nome: "Headset Gamer HyperX",
+        categoria: "Acessórios",
+        preco: 600.00,
+        emEstoque: true
+    }
+];
